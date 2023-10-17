b73f3936231537ee25b5297aace58bdf7e7f8d0b
Spoiler335 <sushantsai8@gmail.com>
2023-10-17 23:07:10 +0530

    Make a blueprint class for Moving Platform

--- Source/ObstacleAssult/MovingPlatform.cpp
+++ Source/ObstacleAssult/MovingPlatform.cpp
@@ -1,27 +1,35 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "MovingPlatform.h"
 
 // Sets default values
 AMovingPlatform::AMovingPlatform()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
 void AMovingPlatform::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	startLocation = GetActorLocation();
 }
 
 // Called every frame
 void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	SetActorLocation(GetActorLocation() + FVector(0, speed * DeltaTime, 0));
 
+	if (GetActorLocation().Y - startLocation.Y > 500)
+	{
+		speed = -speed;
+		startLocation = GetActorLocation();
+	}
+	else if (GetActorLocation().Y - startLocation.Y < -500)
+	{
+		speed = -speed;
+		startLocation = GetActorLocation();
+	}
 }
-
