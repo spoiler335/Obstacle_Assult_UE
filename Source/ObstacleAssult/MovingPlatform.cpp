79189bbe6271b28afc8fd18f8364850f889da274
Spoiler335 <sushantsai8@gmail.com>
2023-10-24 13:14:12 +0530

    Set Collisions for player and setup small obstacles path

--- Source/ObstacleAssult/MovingPlatform.cpp
+++ Source/ObstacleAssult/MovingPlatform.cpp
@@ -2,21 +2,17 @@
 
 #include "MovingPlatform.h"
 
-// Sets default values
 AMovingPlatform::AMovingPlatform()
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 }
 
-// Called when the game starts or when spawned
 void AMovingPlatform::BeginPlay()
 {
 	Super::BeginPlay();
 	startLocation = GetActorLocation();
 }
 
-// Called every frame
 void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
--- Source/ObstacleAssult/MovingPlatform.h
+++ Source/ObstacleAssult/MovingPlatform.h
@@ -12,15 +12,12 @@ class OBSTACLEASSULT_API AMovingPlatform : public AActor
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this actor's properties
 	AMovingPlatform();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 private:
